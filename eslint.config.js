a9307a1a776ada7d563629878fa3b39afb31fb16
Hugo <hugo_mendoza_s@outlook.com>
2025-12-22 20:33:29 -0500

    feat: setup project

---
+++ eslint.config.js
@@ -0,0 +1,38 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
+import { defineConfig, globalIgnores } from 'eslint/config';
+import eslintConfigPrettier from 'eslint-config-prettier';
+import simpleImportSort from 'eslint-plugin-simple-import-sort';
+
+export default defineConfig([
+  globalIgnores(['dist']),
+  {
+    files: ['**/*.{ts,tsx}'],
+    plugins: {
+      'simple-import-sort': simpleImportSort,
+    },
+    extends: [
+      js.configs.recommended,
+      tseslint.configs.recommended,
+      reactHooks.configs.flat.recommended,
+      reactRefresh.configs.vite,
+    ],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+    rules: {
+      'simple-import-sort/imports': [
+        'error',
+        {
+          groups: [['^react', '^@?\\w'], ['^\\.'], ['^.+\\.css$']],
+        },
+      ],
+      'simple-import-sort/exports': 'error',
+    },
+  },
+  eslintConfigPrettier,
+]);
---
+++ src/SimpsonsWebApp.tsx
@@ -0,0 +1,13 @@
+function SimpsonsWebApp() {
+  return (
+    <h1
+      className="text-3xl font-bold underline"
+      role="heading"
+      onClick={() => console.log('Hello World')}
+    >
+      Simpsons Web App
+    </h1>
+  );
+}
+
+export default SimpsonsWebApp;
